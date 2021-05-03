63dd22c3d7d8c30a8bc47c09a0b70bcf80231880
bryanvu82000 <33305885+bryanvu82000@users.noreply.github.com>
2021-05-03 04:56:31 -0700

    initial base code
    code to create sockets and read file

---
+++ fileIO.py
@@ -0,0 +1,35 @@
+"""
+    This file deals with all the functions related to file IO 
+"""
+
+import os
+
+#file paths
+cwd = os.getcwd()
+path_to_file = cwd + "/read_file.txt"
+new_file_path = cwd + "/new_file.txt"
+
+#converts file to bytes to be sent in sockets
+def convert_to_bytes(file_path=path_to_file):
+    read_data = None
+    with open(file_path, 'r') as file:
+        read_data = file.read()
+    return read_data.encode("utf-8")
+
+
+
+def create_file(data):
+    data = data.decode("utf-8")
+    print("Writing to file")
+    with open(new_file_path, 'w') as file:
+        file.write(data)
+    return True
+
+
+def main():
+    data = convert_to_bytes()
+    create_file(data)
+
+
+if __name__ == "__main__":
+    main()
